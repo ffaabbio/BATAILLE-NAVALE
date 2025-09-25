c5298d071f4edeb2d3e76db4dcb2d68a8adfde25
barT089 <koim008@gmail.com>
2025-09-26 01:05:38 +0200

    problemes

--- src/board/board.h
+++ src/board/board.h
@@ -2,6 +2,7 @@
 #ifndef DEF_HEADER_BOAT
 #define DEF_HEADER_BOAT
 
-char** createMap();
+char** createMap(int rows,int cols);
+void display_map(char** map,int rows,int cols);
 
 #endif
--- src/main.c
+++ src/main.c
@@ -8,14 +8,14 @@
 int main(void) {
     int rows,cols;
     while (1) {
-        printf("Enter the number of rows (1-99): ");
+        printf("Enter the number of rows (1-20): ");
         if (scanf("%d", &rows) != 1) {
             printf("Invalid input! Please enter a number.\n");
             while (getchar() != '\n');
             continue;
         }
 
-        printf("Enter the number of cols (1-99): ");
+        printf("Enter the number of cols (1-20): ");
         if (scanf("%d", &cols) != 1) {
             printf("Invalid input! Please enter a number.\n");
             while (getchar() != '\n');
@@ -26,11 +26,14 @@ int main(void) {
             break;
         }
 
-        printf("Please enter numbers between 1 and 99.\n");
+        printf("Please enter numbers between 1 and 20.\n");
     }
     printf("Veuillez rentrer la taille des plateaux\n");
     char ** board1 = createMap(rows,cols);
+    display_map(board1, rows, cols);
+    printf("\n");
     char ** board2 = createMap(rows,cols);
+    display_map(board2, rows, cols);
     Boat *boat1 = malloc(sizeof(Boat) * 3);
     Boat *boat2 = malloc(sizeof(Boat) * 3);
     get_position_of_cheap(board1, board2, rows, cols, boat1, boat2);
