2b38ee3a2691ec333e1929e853bb02ef45aa4909
barT089 <koim008@gmail.com>
2025-09-26 06:53:17 +0200

    deizd

--- src/main.c
+++ src/main.c
@@ -38,5 +38,18 @@ int main(void) {
     Boat *boat2 = malloc(sizeof(Boat) * 3);
     get_position_of_cheap(board1, board2, rows, cols, boat1, boat2);
 
+
+    while(1) {
+
+
+        printf("PLAYER 1 >>>>>>>>");
+        test(board2,rows,cols);
+        printf("PLAYER 2 >>>>>>>>");
+        test(board1,rows,cols);
+
+
+    }
+
+
     return 0;
 }
